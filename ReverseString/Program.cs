2e2da49ba2dcbca7ce01d257b42e01327c7414ac
CapioIM <131615709+CapioIM@users.noreply.github.com>
2023-06-01 22:45:52 +0100

    Add project files.

---
+++ ReverseString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Weolcome to Reverse String Programm");
+            string userInput = Console.ReadLine();
+
+            for (int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userInput[i]);
+            }
+        }
+    }
+}
